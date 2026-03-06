7df214807b512fbf66c023fbb1f8b4a966fb3e39
RAFAEL YU <25254161@educ123.sp.senai.br>
2026-03-06 10:54:30 -0300

    Add Carousel Function and Improved Footer

--- js/index.js
+++ js/index.js
@@ -7,3 +7,34 @@ window.addEventListener("scroll", () => {
         scrollTopBtn.classList.add("hidden");
     }
 });
+
+const carousel = document.querySelector(".carousel_images");
+const images = document.querySelectorAll(".carousel_images img");
+const next = document.querySelector(".carousel_buttons.right");
+const prev = document.querySelector(".carousel_buttons.left");  
+
+let index = 0;
+const visible = 5;
+
+function updateCarousel() {
+    const move = 100 / 5;
+    carousel.style.transform = `translateX(-${index * move}%)`;
+}
+
+next.addEventListener("click", () => {
+    if(index < images.length - visible){
+        index++;
+    } else {
+        index = 0;
+    }
+    updateCarousel();
+});
+
+prev.addEventListener("click", () => {
+    if(index > 0){
+        index--;
+    } else {
+        index = images.length - visible;
+    }
+    updateCarousel();
+});
